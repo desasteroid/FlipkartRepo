f3d158c5f8e4c35fd3cdf37d6c615679de0560de
Kumar Suman Saurabh <11suman00@gmail.com>
2023-06-19 15:42:11 +0530

    My first commit

---
+++ src/com/flipkart/demo/DemoTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * 
+ */
+public class DemoTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, this is my first app");
+
+	}
+
+}
